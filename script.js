c4b8382c64ddca61cf9a8da7af808fc40d865473
JC-JoaoC <joaoc0963@gmail.com>
2023-10-03 13:17:19 -0300

    finish!

--- script.js
+++ script.js
@@ -1,57 +1,69 @@
 const state = {
-    view: {
-        squares: document.querySelectorAll(".square"),
-        enemy: document.querySelector(".enemy"),
-        time: document.querySelector ("#time"),
-        score: document.querySelector ("#score")
-    },
-    values: {
-        timeID: setInterval(randomSquare, 1000),
-        countDownTimerID: setInterval(countDown, 1000),
-        gameVelocity: 1000,
-        hitPosition: 0,
-        result: 0,
-        tempo: 60,
-    },
-};
+  view: {
+    squares: document.querySelectorAll(".square"),
+    enemy: document.querySelector(".enemy"),
+    timeLeft: document.querySelector("#time-left"),
+    score: document.querySelector("#score"),
+  },
+  values: {
+    gameVelocity: 500,
+    hitPosition: 0,
+    result: 0,
+    currentTime: 60,
+    countDown: 1000,
+  },
+  action: {
+    timeID: null,
+    countDownTimerID: null,
+  },
+}
 
 function countDown() {
-    state.values.tempo--;
-    state.view.timeLeft.textContent = state.values.tempo;
-    if (state.values.countDownTimerID <= 0) {
-        window.alert("Game Over! O seu resultado foi:" + state.values.result);
+  state.action.countDownTimerID = setInterval(() => {
+    state.values.currentTime--
+    state.view.timeLeft.textContent = state.values.currentTime
+    if (state.values.currentTime <= 0) {
+      clearInterval(state.action.countDownTimerID)
+      clearInterval(state.action.timeID)
+      alert("Tempo esgotado! O seu resultado foi: " + state.values.result)
     }
+  }, 1000)
 }
 
+function moveEnemy() {
+  state.values.timeID = setInterval(randomSquare, state.values.gameVelocity)
+}
 function randomSquare() {
-    state.view.squares.forEach((square) => {
-        square.classList.remove("enemy");
-});
-    let randomNumber = Math.floor(Math.random() * 9);
-    let randomSquare = state.view.squares[randomNumber];
-    randomSquare.classList.add("enemy");
-    state.values.hitPosition = randomSquare.id
+  state.view.squares.forEach((square) => {
+    square.classList.remove("enemy")
+  })
+  let randomNumber = Math.floor(Math.random() * 9)
+  let randomSquare = state.view.squares[randomNumber]
+  randomSquare.classList.add("enemy")
+  state.values.hitPosition = randomSquare.id
+}
+function addListenerHitBox() {
+  state.view.squares.forEach((square) => {
+    square.addEventListener("mousedown", () => {
+      if (square.id === state.values.hitPosition) {
+        state.values.result++
+        state.view.score.textContent = state.values.result
+        state.values.hitPosition = null
+        PlayerSound()
+      }
+    })
+  })
 }
 
-// function moveEnemy() {
-//     state.values.timeID = setInterval(randomSquare, state.values.gameVelocity);
-// }
-
-function addListenerHitBox() {
-    state.view.squares.forEach((square) => {
-        square.addEventListener("mousedown", () => {
-            if (square.id === state.values.hitPosition) {
-                state.values.result++;
-                state.view.score.textContent = state.values.result;
-                state.values.hitPosition = null;
-            }
-        });
-    });
+function PlayerSound() {
+  let audio = new Audio("./audio/som.m4a")
+  audio.volume = 0.2
+  audio.play()
 }
 
 function init() {
-    // moveEnemy();
-    addListenerHitBox();
-};
-
-init();
+  moveEnemy()
+  addListenerHitBox()
+  randomSquare()
+  countDown()
+}
